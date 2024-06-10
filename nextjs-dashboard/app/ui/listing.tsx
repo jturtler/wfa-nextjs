7ec29643912ef4d14a161ac64ef7567bd0f9a84c
James Chang <jamesbchang@gmail.com>
2024-06-10 13:59:01 +0900

    menu sidebar

--- nextjs-dashboard/app/ui/listing.tsx
+++ nextjs-dashboard/app/ui/listing.tsx
@@ -1,14 +1,12 @@
 'use client';
 
 import { useEffect, useState } from "react";
-import { useLoginUserContext } from "../contexts/loginUserContext";
 import ClientCard from "./clientCard";
+import SectionTop from "./sectionTop";
 
 export default function Listing() {
 
-	const { username } = useLoginUserContext();
 	const [ clientList, setClientList ] = useState<Array<any>>([]);
-
 	//const clientList = [];
 
 	const getClientsList = () => {
@@ -33,15 +31,15 @@ export default function Listing() {
 
 	return (
 	<div className="h-[100vh] overflow-hidden">
-		<div className="divTopNav h-[30px] bg-blue-300 p-1 text-white">INFO - { username }</div>
+		<SectionTop></SectionTop>
 		<div className="divMiddleContent flex">
-		<div className="divSiceNav w-10 hidden bg-gray-700 text-gray-300 p-1">m1</div>
-		<div className="divMainList m-1 grid h-[calc(100vh-68px)] flex-1 content-start gap-1 overflow-x-auto border-0 border-gray-400 md:grid-cols-2">
-			{ clientList?.map( (client, index) => (
-				<ClientCard key={index} client={client}></ClientCard>
-			))
-			}
-		</div>
+			<div className="divSiceNav w-10 hidden bg-gray-700 text-gray-300 p-1">m1</div>
+			<div className="divMainList m-1 grid h-[calc(100vh-68px)] flex-1 content-start gap-1 overflow-x-auto border-0 border-gray-400 md:grid-cols-2">
+				{ clientList?.map( (client, index) => (
+					<ClientCard key={index} client={client}></ClientCard>
+				))
+				}
+			</div>
 		</div>
 		<div className="divBottomTop h-[30px] bg-gray-900 p-1 text-xs text-white">Version 1.2.0</div>
 	</div>

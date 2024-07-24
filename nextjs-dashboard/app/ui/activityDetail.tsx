516070418773fbf634ead5d8709bd26d8c1db6fa
James Chang <jamesbchang@gmail.com>
2024-07-24 11:07:54 +0900

    Update activityDetail.tsx

--- nextjs-dashboard/app/ui/activityDetail.tsx
+++ nextjs-dashboard/app/ui/activityDetail.tsx
@@ -1,14 +1,22 @@
 import { useState } from "react";
 import SectionTop from "./sectionTop";
+import { Button } from "./button";
 
 export default function ActivityDetail( { onClose, activity }: { onClose: () => void, activity: any } ) {
 
 	const [ activeTab, setActiveTab ] = useState( 'tab1' );
+	const [ btnClickCount, setbtnClickCount ] = useState( 0 );
 
 	//  (e) => ActivityDetailModalClose()
 
 	const tabStyleActive = 'border-b-2 border-blue-500  bg-blue-100 text-blue-600';
 	const tabStyleInactive = 'bg-gray-100 text-gray-500 hover:text-gray-700';
+
+	const btnClickEvent = () => {
+
+		// Increment the state
+		setbtnClickCount( btnClickCount + 1 );
+	};
 
 	return ( 
 		<div className="bg-white w-screen h-screen">
@@ -21,7 +29,10 @@ export default function ActivityDetail( { onClose, activity }: { onClose: () => 
 				
 			</div>
 			<div className="bg-orange-50 p-4">
-				TEST
+				<Button className="flex-1 justify-center mx-5" onClick={ (e) => { btnClickEvent() } }>
+							Click
+						</Button>
+				<div>Click Count: { btnClickCount }</div>
 			</div>
 		</div>
 	</div>
